930f24174561ff38eacb56bd97f0aeb1ea124952
FensRah <raherimihajahasiniaina@gmail.com>
2025-08-11 10:07:36 +0300

    delete pagination if there is no data in interim table

--- src/ui/components/interimaire/missions/search/InterimaireMissionsTable.js
+++ src/ui/components/interimaire/missions/search/InterimaireMissionsTable.js
@@ -42,12 +42,12 @@ const tenantID = +process.env.REACT_APP_TENANT_ID;
 const baseDate = new Date();
 baseDate.setMonth(baseDate.getMonth() - 1);
 // eslint-disable-next-line no-extend-native
-Array.constructor.prototype.flexFilter = function (info) {
+Array.constructor.prototype.flexFilter = function(info) {
   var matchesFilter,
     matches = [],
     count;
 
-  matchesFilter = function (item) {
+  matchesFilter = function(item) {
     count = 0;
     for (var n = 0; n < info.length; n++) {
       if (info[n]["Values"].indexOf(item[info[n]["Field"]]) > -1) {
@@ -873,7 +873,7 @@ function InterimaireMissionsTable({ refresh }) {
               </div>
             )}
           </Row>
-          {missions && missions.length > 0 &&
+          {missions && missions.length > 0 && (
             <div style={{ marginTop: 30 }}>
               <RemotePagination
                 data={missions}
@@ -882,7 +882,8 @@ function InterimaireMissionsTable({ refresh }) {
                 totalSize={totalCount}
                 onTableChange={handleTableChange}
               />
-            </div>}
+            </div>
+          )}
         </div>
       )}
     </>
