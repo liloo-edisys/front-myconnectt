e9cf062c472a79344ab3702c5484505cfaa6e8a8
FensRah <raherimihajahasiniaina@gmail.com>
2025-09-04 13:33:36 +0300

    fix suivi date

--- src/ui/components/client/customer-order/CustomerOrder.js
+++ src/ui/components/client/customer-order/CustomerOrder.js
@@ -426,14 +426,20 @@ function CustomerOrder(props) {
                     <div className="d-flex flex-column">
                       <span className="text-dark font-weight-bold font-size-h5">
                         Créée le{" "}
-                        {new Date(
-                          activeCustomerOrder.vacancy.creationDate
-                        ).toLocaleDateString("fr-FR", {
-                          day: "numeric",
-                          month: "long",
-                          year: "numeric"
-                        })}
+                        {activeCustomerOrder?.vacancy?.creationDate
+                          ? new Date(
+                              activeCustomerOrder.vacancy.creationDate.replace(
+                                /(\.\d{3})\d+$/,
+                                "$1"
+                              )
+                            ).toLocaleDateString("fr-FR", {
+                              day: "numeric",
+                              month: "long",
+                              year: "numeric"
+                            })
+                          : "—"}
                       </span>
+
                       <span className="text-dark font-weight-bold font-size-h5">
                         {activeCustomerOrder.vacancy.vacancyNumberOfJobs -
                           activeCustomerOrder.vacancy
